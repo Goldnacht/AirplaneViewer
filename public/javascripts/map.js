3c4c48a030bf373c2e9cf80dc918d20feba541d3
Frederik Eschmann <frederik.eschmann@gmail.com>
2015-04-13 20:26:44 -0700

    - simple Airplane display

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -22,13 +22,32 @@ var map = new ol.Map({
     })
 });
 
-function Airplane(icao) {
-    this.icao = icao;
-}
-
 var airplanes = [];
 
-function addAirplane(data) {
+function updateAirplane(data) {
+    displayAirplane(data);
+    var updated = false;
+    var ap = null;
+    for (ap in airplanes) {
+        if (ap.icao == data.icao) {
+            ap.latitude = data.latitude;
+            ap.longitude = data.longitude;
+
+            updated = true;
+        }
+    }
+    if (!updated) {
+        airplanes.push({
+            icao: data.icao,
+            latitude: data.latitude,
+            longitude: data.longitude
+        });
+    }
+}
+
+//{\"icao\": \"3C70A9\", \"timestamp\": \"1428977046.2168989\", \"id\": \"BCS6378\", \"latitude\": 1.1145172119140625, \"longitude\": 0.10944431110963983, \"altitude\": 37025, \"heading\": , \"horspeed\": 496, \"verspeed\": 0}"]}
+
+function displayAirplane(data) {
     var iconFeature = new ol.Feature({
         geometry: new ol.geom.Point(ol.proj.transform([data.latitude, data.longitude], 'EPSG:4326', 'EPSG:3857')),
         name: data.icao,
@@ -50,6 +69,16 @@ function subscribe() {
     });
 }
 function main() {
+    $.get("/flightdata.json",function(data, state) {
+        var ap;
+        for(ap in data.airplanes) {
+            updateAirplane(data.airplanes[ap]);
+        }
+    },"json");
 
+    /*var airplane;
+    for(airplane in airplanes) {
+        displayAirplane(airplane);
+    }*/
 }
 main();
