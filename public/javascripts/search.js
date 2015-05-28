c498f320649a5edcaa1fc4d30669ef00b3c4c0e5
Christine Vosseler <chvoit02@hs-esslingen.de>
2015-05-28 13:14:03 +0200

    searchIcao created

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -1,9 +1,19 @@
 function searchAirplane(formName){
     var searchString = $(formName).find(".searchInput").val();
-    alert(searchString);
+    var airplane=searchIcao(searchString);
+    if(airplane){
+        setFocusOnAirplane(airplane);
+    }
 }
 
-
 function searchIcao(icao){
+    for(ap in airplanes){
+        var airplane=airplanes[ap];
+        if (airplane.icao == icao) return airplane;
+    }
+    return null;
+}
+
+function setFocusOnAirplane (airplane){
 
 }
